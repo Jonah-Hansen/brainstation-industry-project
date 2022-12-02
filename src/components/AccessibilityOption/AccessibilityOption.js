94e40a3e60c66cf7631828ce3986dd0ebf6f24d4
Jonah-Hansen <tce_mc@telus.net>
2022-12-02 04:04:05 -0600

    structure and some styling for modal

---
+++ src/components/AccessibilityOption/AccessibilityOption.js
@@ -0,0 +1,33 @@
+import Switch from '@mui/material/Switch';
+import './AccessibilityOption.scss';
+
+function AccessibilityOption({ type, title, description, icon }) {
+
+  const colors = ['#DB4437', '#5592F6', '#F6C333', '#57BB8B', '#5E409D', '#FF67D4', '#13662B', '#068D85']
+
+  return (
+
+    <div className='option-row'>
+      {type === 'colors' ?
+        <>
+          <h2 className='option-row__title'>{title}</h2>
+          <Switch />
+          <div className='option-row__colors'>
+            {colors.map(color => <div key={color} style={{ backgroundColor: color }} className={`option-row__color`} />)}
+          </div>
+        </>
+        :
+        <>
+          <img className='option-row__icon' src={icon} alt={`${title} icon`} />
+          <hgroup className='option-row__details'>
+            <h2 className='option-row__title'>{title}</h2>
+            <p className='option-row__description'>{description}</p>
+          </hgroup>
+          <Switch />
+        </>
+      }
+    </div>
+  )
+}
+
+export default AccessibilityOption
